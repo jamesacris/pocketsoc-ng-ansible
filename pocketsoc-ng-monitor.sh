4599f9693717245a529463a0e3633c1166007d87
drmcrooks <David.crooks@stfc.ac.uk>
2022-06-20 12:17:22 +0200

    Update pocketsoc-ng-monitor.sh

--- pocketsoc-ng-monitor.sh
+++ pocketsoc-ng-monitor.sh
@@ -2,4 +2,15 @@
 
 scschosts=`cat hosts | grep scsc`
 
-echo $scschosts
+for scschost in $scschosts; do
+    echo -n MISP:
+    echo yes | openssl s_client -host $scschost -port 443 1> /dev/null 2> /dev/null ; echo $?
+
+    echo -n OpenSearch:
+    echo yes | openssl s_client -host $scschost -port 8443 1> /dev/null 2> /dev/null ; echo $?
+
+    echo -n Portainer:
+    echo yes | openssl s_client -host $scschost -port 9443 1> /dev/null 2> /dev/null ; echo $?
+
+    echo --------
+done
